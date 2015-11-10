dc74aafd0b26e81668f8e651dc35b8056882ece7
Yash LINUX <code.yash@gmail.com>
2015-11-10 21:32:07 +0530

    Expand now working

--- emmet/test.js
+++ emmet/test.js
@@ -1,7 +1,9 @@
 var api = require('./../api');
 
 var plugin = require('./plugin');
+
 var expand = require('./expand');
+expand = expand()
 
 var client = api();
 
@@ -10,31 +12,25 @@ function cal(res)
 	console.log("ff" + res + "||" + a.response);
 }
 
-//plugin = plugin()
 
-//plugin.yash();
-
-
-expand = expand()
 var v = expand.expand('ul+',{});
 
 console.log( "V: " + v );
-//a.request("hello",[],cal);
+//a.request("hello",[],cal);  div.class
 
 //a.request("selectedText",[],cal);
 
-client.request('hello',[], function(err, errors, res){
+client.request('selectedText',[], function(err, errors, res){
     
     if(err)
     {
-        this.error = errors; 
+        this.error = errors;
+        console.log('Err:' + errors); 
     }
+    
+    var v = expand.expand(res, {});
 
-    console.log("Result: " + res);
-
-
-
-
+    console.log("Result: " + res + " | " + v);
 
 
 
