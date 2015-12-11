1a5fbd5b34c11f021c47b9891b163bf9454d58da
Yash <yash@speedovation.com>
2015-12-11 16:03:37 +0530

    js beauty added
    full refinement is awaited

---
+++ jsbeauty/index.js
@@ -0,0 +1,54 @@
+var api = require('./../api');
+api = api();
+
+var beautify_js = require('js-beautify'); 
+var beautify_css = require('js-beautify').css;
+var beautify_html = require('js-beautify').html;
+//res = jsbeautifier.beautify('your javascript string')
+//res = jsbeautifier.beautify_file('some_file.js')
+//...or, to specify some options:
+
+//opts = jsbeautifier.default_options()
+//opts.indent_size = 2
+//res = jsbeautifier.beautify('some javascript', opts)
+
+
+
+var funcstr = process.argv[2];
+var jsonstr = process.argv[3]
+
+//console.log(funcstr + "|" + jsonstr);
+
+var api_functions = JSON.parse([jsonstr]);
+
+var x = { }; // better would be to have module create an object
+
+function return_result(method,params)
+{
+	console.log('{ "method" : "' + method + '", "params" : ["'+ params +'"]}')
+}
+
+
+x.format_html = function() { 
+
+    //check if selected_text found
+	// if not call a json rpc and get full text and
+	// set it using
+	
+	//api_functions.selected_text
+	
+	var res = beautify_html("<html><head><meta charset='UTF-8' /><title>Document</title></head><body></body></html>");
+    
+    return return_result('replace_selected_text',[res]);
+
+}
+
+x.format_js = function() { 
+
+    var res = beautify_js("<html><head><meta charset='UTF-8' /><title>Document</title></head><body></body></html>");
+    
+    return return_result('replace_selected_text',[res]);
+
+}
+
+x[funcstr]();
