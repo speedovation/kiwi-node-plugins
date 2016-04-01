bc63000f145b86072d5fe92dbd992be28d83558e
Yash <yash@speedovation.com>
2016-04-02 02:14:58 +0530

    Seperated Core

--- application.js
+++ application.js
@@ -1,44 +1,8 @@
-api = require('./src/Plugins/api');
+api = require('./src/Core/Api');
 jsesc = require('jsesc');
 api = api();
 
-//Sample only
-//var learn = require('./src/Plugins/learn');
-//learn.requestFlush(); 
-
-
-var funcstr;
-api_functions= {};
-var moduleName;
-
-var program = require('commander');
-
-program
-    .version('0.0.1')
-    .usage(
-'\n\n' + 
-'___ ____ _ ____ ____ _ ______________\n'+
-'|_  ||_  _|  (_)|_  _|    |_  _|(_)  \n'+
-'  | |_/ /    __   \\ \\  /\\  / /  __   \n'+
-"  |  __'.   [  |   \\ \\/  \\/ /  [  |  \n"+
-' _| |  \\ \\_  | |    \\  /\\  /    | |  \n'+
-'|____||____|[___]    \\/  \\/    [___] \n'+
-'\n\n' +
-"<module> <func> {data}")
-
-    .arguments('<module> <func> {data}')
-    .action(function(module,func, data) {
-        moduleName = module;
-        funcstr = func;
-        api_functions = data;
-    });
-
-program.parse(process.argv);
-
-if (typeof funcstr === 'undefined') {
-    console.error('no command given!');
-    process.exit(1);
-}
+CommandHandler = require('./src/Core/CommandHandler');
 
 
 var module = require('./src/Plugins/'+ moduleName )
---
+++ src/Core/CommandHandler/index.js
@@ -0,0 +1,37 @@
+//Sample only
+//var learn = require('./src/Plugins/learn');
+//learn.requestFlush(); 
+
+
+funcstr = '';
+api_functions= {};
+moduleName = '';
+
+var program = require('commander');
+
+program
+    .version('0.0.1')
+    .usage(
+'\n\n' + 
+'___ ____ _ ____ ____ _ ______________\n'+
+'|_  ||_  _|  (_)|_  _|    |_  _|(_)  \n'+
+'  | |_/ /    __   \\ \\  /\\  / /  __   \n'+
+"  |  __'.   [  |   \\ \\/  \\/ /  [  |  \n"+
+' _| |  \\ \\_  | |    \\  /\\  /    | |  \n'+
+'|____||____|[___]    \\/  \\/    [___] \n'+
+'\n\n' +
+"<module> <func> {data}")
+
+    .arguments('<module> <func> {data}')
+    .action(function(module,func, data) {
+        moduleName = module;
+        funcstr = func;
+        api_functions = data;
+    });
+
+program.parse(process.argv);
+
+if (typeof funcstr === 'undefined') {
+    console.error('no command given!');
+    process.exit(1);
+}
