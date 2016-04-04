557758cc792d34dc9c6f042dc38d3f38ac6cbcc8
Yash <yash@speedovation.com>
2016-04-04 16:30:48 +0530

    Html Lint added

--- src/Plugins/lint/index.js
+++ src/Plugins/lint/index.js
@@ -24,14 +24,14 @@ lint.lint_js = function() {
 
 
         var options = {
-            undef: true ,
+            undef: true,
             globals: true
-            //,"node": true
+                //,"node": true
         };
         var predef = {
             api: false,
-            logger : false,
-            jsesc : false
+            logger: false,
+            jsesc: false
         };
 
         //JSHINT(res, options, predef);
@@ -40,33 +40,32 @@ lint.lint_js = function() {
 
         var yerrors = jshint.JSHINT.data().errors;
         var modifiedErrors = {};
-        if( !underscore.isEmpty(yerrors) )
-        {
-        
-        
-          yerrors.forEach(function(err) {
-            logger.debug(err.line, err.message, err.reason);
-            modifiedErrors[err.line - 1 ] = err.message +" "+ err.reason;
-            //console.log(err);
-          });
-        
+        if (!underscore.isEmpty(yerrors)) {
+
+
+            yerrors.forEach(function(err) {
+                logger.debug(err.line, err.message, err.reason);
+                modifiedErrors[err.line - 1] = err.message + " " + err.reason;
+                //console.log(err);
+            });
+
         }
 
 
         //console.log(jshint.JSHINT.data());
-        
+
         modifiedErrors = JSON.stringify(modifiedErrors);
 
         //m = '{"2" : "some error msg"}';
         //modifiedErrors = jsesc(modifiedErrors, {
         //    'quotes': 'double'
         //});
-        
-        
-        
+
+
+
         //logger.debug(api.return_result('set_markers', [m]));
 
-        
+
         return api.return_result('set_markers', [modifiedErrors]);
 
     });
@@ -95,39 +94,38 @@ lint.lint_css = function() {
 
 
         var result = cssLint.verify(res);
-        
-        
+
+
         var modifiedErrors = {};
-        
-        
+
+
         if (result.messages.length === 0) {
             //Success
         } else {
             //Errors or warnings
-            
+
             var i = 0;
-            for (i=0; i<result.messages.length; i++) 
-            {
+            for (i = 0; i < result.messages.length; i++) {
                 var message = result.messages[i];
                 logger.debug("%s (line %d, col %d): %s", message.type, message.line, message.col, message.message);
-                
-                modifiedErrors[message.line - 1 ] =  message.type.toUpperCase() +" : "+ message.message  ;
+
+                modifiedErrors[message.line - 1] = message.type.toUpperCase() + " : " + message.message;
             }
         }
 
         //console.log(jshint.JSHINT.data());
-        
+
         modifiedErrors = JSON.stringify(modifiedErrors);
 
         //m = '{"2" : "some error msg"}';
         //modifiedErrors = jsesc(modifiedErrors, {
         //    'quotes': 'double'
         //});
-        
-        
+
+
         //logger.debug(api.return_result('set_markers', [m]));
 
-        
+
         return api.return_result('set_markers', [modifiedErrors]);
 
     });
@@ -153,28 +151,27 @@ lint.lint_json = function() {
             console.log('Err:' + errors);
             return;
         }
-		
-		var modifiedErrors = {};
 
-		try{
-		
-        	jsonlint.parse(res);
-		
-		}
-		catch(err) {
-   			//console.log(err.message, err.name);
-			
-			 //'Parse error on line 1:\n/* jshint node: true\n^\nExpecting \'STRING\', \'NUMBER\', \'NULL\', \'TRUE\', \'FALSE\', \'{\', \'[\', got \'undefined\'' }
+        var modifiedErrors = {};
 
-			var line = err.message.match(/(line\s*[0-9]+)/g)[0].split(" ")[1];
-			
-			modifiedErrors[line - 1 ] =  err.message.replace(/(\r\n|\n|\r)/gm,"");
+        try {
 
-			//console.log(util.inspect(response, true, null));
-		}
-        
-        
-        
+            jsonlint.parse(res);
+
+        } catch (err) {
+            //console.log(err.message, err.name);
+
+            //'Parse error on line 1:\n/* jshint node: true\n^\nExpecting \'STRING\', \'NUMBER\', \'NULL\', \'TRUE\', \'FALSE\', \'{\', \'[\', got \'undefined\'' }
+
+            var line = err.message.match(/(line\s*[0-9]+)/g)[0].split(" ")[1];
+
+            modifiedErrors[line - 1] = err.message.replace(/(\r\n|\n|\r)/gm, "");
+
+            //console.log(util.inspect(response, true, null));
+        }
+
+
+
         //modifiedErrors = JSON.stringify(modifiedErrors);
 
         //m = '{"2" : "some error msg"}';
@@ -182,11 +179,11 @@ lint.lint_json = function() {
             'quotes': 'double',
             'compact': true
         });
-        
-        
+
+
         //logger.debug(modifiedErrors);
 
-        
+
         return api.return_result('set_markers', [modifiedErrors]);
 
     });
@@ -214,51 +211,147 @@ lint.lint_xml = function() {
         }
 
 
-        
-        var modifiedErrors = {};
-        var xmlDoc ;
 
-        try{
-            
+        var modifiedErrors = {};
+        var xmlDoc;
+
+        try {
+
             //xmlDoc = libxmljs.parseXmlString(res);
 
-        /*if (xmlDoc.validationErrors.length === 0) {
-            //Success
-        } else { }*/
-        
-        
-        }
-        catch(err) 
-        {
-        
+            /*if (xmlDoc.validationErrors.length === 0) {
+                //Success
+            } else { }*/
+
+
+        } catch (err) {
+
             //Errors or warnings
-            
-           /* var i = 0;
-            for (i=0; i<xmlDoc.validationErrors.length; i++) 
-            {
-                var error = xmlDoc.validationErrors[i];
-                //logger.debug("%s (line %d, col %d): %s", error.type, error.line, error.col, error.message);
-                
-                //modifiedErrors[message.line - 1 ] =  message.type.toUpperCase() +" : "+ message.message  ;
-                
-                
-                console.log(error);
-            }*/
+
+            /* var i = 0;
+             for (i=0; i<xmlDoc.validationErrors.length; i++) 
+             {
+                 var error = xmlDoc.validationErrors[i];
+                 //logger.debug("%s (line %d, col %d): %s", error.type, error.line, error.col, error.message);
+                 
+                 //modifiedErrors[message.line - 1 ] =  message.type.toUpperCase() +" : "+ message.message  ;
+                 
+                 
+                 console.log(error);
+             }*/
         }
 
         //console.log(jshint.JSHINT.data());
-        
+
         modifiedErrors = JSON.stringify(modifiedErrors);
 
         //m = '{"2" : "some error msg"}';
         //modifiedErrors = jsesc(modifiedErrors, {
         //    'quotes': 'double'
         //});
-        
-        
+
+
         //logger.debug(api.return_result('set_markers', [m]));
 
-        
+
+        return api.return_result('set_markers', [modifiedErrors]);
+
+    });
+
+
+};
+
+
+var HTMLHint = require("htmlhint").HTMLHint;
+
+lint.lint_html = function() {
+
+    //TODO:  Test selected_text  	//api_functions.selected_text
+
+    //if selected_text not found call a json rpc and get full text and
+    api.request('text', [], function(err, errors, res) {
+
+
+
+        if (err) {
+            this.error = errors;
+            console.log('Err:' + errors);
+            return;
+        }
+
+
+
+        var modifiedErrors = {};
+
+        var messages;
+
+
+
+        //xmlDoc = libxmljs.parseXmlString(res);
+
+        var options = {
+            'title-no-dup': true,
+            "tagname-lowercase": true,
+            "attr-lowercase": true,
+            "attr-value-double-quotes": true,
+            "doctype-first": true,
+            "tag-pair": true,
+            "spec-char-escape": true,
+            "id-unique": true,
+            "src-not-empty": true,
+            "attr-no-duplication": true,
+            "title-require": true,
+            "csslint" : {
+                            "display-property-grouping": true,
+                            "known-properties": true,
+                            "important" :true
+                        },
+            "jshint" :  { 
+                            undef: true,
+                            globals: true 
+                        }
+            
+        };
+
+
+        messages = HTMLHint.verify(res, options);
+
+        //console.log(messages);
+
+        //console.log(messages.length);
+
+        if (messages.length === 0) {
+            //Success
+        } else {
+            //console.log(messages);
+
+            for (var i = 0; i < messages.length; i++) {
+                var error = messages[i];
+                logger.debug("%s (line %d, col %d): %s", error.type, error.line, error.col, error.message);
+
+                modifiedErrors[error.line - 1] = error.type.toUpperCase() + " : " + error.message + error.evidence;
+
+
+                //console.log(error);
+            }
+        }
+
+
+
+
+        //console.log(jshint.JSHINT.data());
+
+        modifiedErrors = JSON.stringify(modifiedErrors);
+
+        //m = '{"2" : "some error msg"}';
+        //modifiedErrors = jsesc(modifiedErrors, {
+        //    'quotes': 'double'
+        //});
+
+
+        //logger.debug(api.return_result('set_markers', [m]));
+
+
         return api.return_result('set_markers', [modifiedErrors]);
 
     });
